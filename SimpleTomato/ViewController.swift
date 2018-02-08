fc983760a3c9fd1ed7a1892873162ed73b6b456d
juncollin <juncollin@gmail.com>
2018-02-09 00:12:28 +0900

    Initial Commit

---
+++ SimpleTomato/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleTomato
+//
+//  Created by 有本淳吾 on 2018/02/09.
+//  Copyright © 2018年 有本淳吾. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
